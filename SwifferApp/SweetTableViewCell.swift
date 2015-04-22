7658b824e2513c6637f120fb40dd6d33d65c0ed8
Rachel <rachel@rachelrockaway.com>
2015-04-22 14:39:35 -0400

    Create and connect SweetTableViewCell and recreate ComposeView Controller text view so that text starts at top of view

---
+++ SwifferApp/SweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SweetTableViewCell.swift
+//  SwifferApp
+//
+//  Created by Rachel Stevenson on 4/22/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+import UIKit
+
+class SweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
