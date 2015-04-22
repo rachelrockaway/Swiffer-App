a847e62545a662c1eb6788f169002cc8be42c2f0
Rachel <rachel@rachelrockaway.com>
2015-04-22 14:28:03 -0400

    Add IBOutlets for ComposeViewController text view and label and IBAction for done bar button

---
+++ SwifferApp/ComposeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ComposeViewController.swift
+//  SwifferApp
+//
+//  Created by Rachel Stevenson on 4/22/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    @IBOutlet weak var sweetTextView: UITextView!
+    
+    @IBOutlet weak var characterRemainingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendSweet(sender: UIBarButtonItem) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
